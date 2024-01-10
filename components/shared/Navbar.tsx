9ffb91d81236bcd6303fcc361c10012a663802f3
Folka2134 <folka2134@gmail.com>
2024-01-10 22:38:56 +0000

    updated mobile navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,21 +2,35 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
+import { FilmIcon } from "@heroicons/react/24/solid";
+import { Squares2X2Icon } from "@heroicons/react/24/solid";
+import { TvIcon } from "@heroicons/react/24/solid";
+import { BookmarkIcon } from "@heroicons/react/24/solid";
+
 const Navbar = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex items-center justify-between bg-[#161D2F]">
-        <Link href="/" className="w-36">
-          <p>Home</p>
+      <div className="wrapper flex h-20 items-center justify-between bg-[#161D2F] px-6">
+        <Link href="/">
+          <FilmIcon className="h-10 w-10 fill-[#FD4746]" />
         </Link>
 
-        {/* <SignedIn>
-          <nav className="md:flex-between hidden w-full max-w-xs">
-            <NavItems />
-          </nav>
-        </SignedIn> */}
+        <nav className="flex w-full items-center justify-center gap-5">
+          <Link href="/">
+            <Squares2X2Icon className="h-8 w-8 fill-[#5B6990] transition-colors duration-200 hover:fill-white" />
+          </Link>
+          <Link href="/">
+            <FilmIcon className="h-8 w-8 fill-[#5B6990] transition-colors duration-200 hover:fill-white" />
+          </Link>
+          <Link href="/">
+            <TvIcon className="h-8 w-8 fill-[#5B6990] transition-colors duration-200 hover:fill-white" />
+          </Link>
+          <Link href="/">
+            <BookmarkIcon className="h-8 w-8 fill-[#5B6990] transition-colors duration-200 hover:fill-white" />
+          </Link>
+        </nav>
 
-        <div className="flex w-32 justify-end gap-3">
+        <div className="flex justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
