5e35bf0882ce0796bfb3aa34b407cd6f059ac662
Folka2134 <folka2134@gmail.com>
2024-01-10 22:03:54 +0000

    initial commit

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,34 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between bg-[#161D2F]">
+        <Link href="/" className="w-36">
+          <p>Home</p>
+        </Link>
+
+        {/* <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn> */}
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <button className="rounded-full">
+              <Link href="/sign-in">Login</Link>
+            </button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
