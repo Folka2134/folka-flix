ee99a0ed7cebc41701cf8d03301f7242ca13e414
Folka2134 <folka2134@gmail.com>
2024-01-11 11:02:57 +0000

    created search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Input } from "./ui/input";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="flex items-center">
+      <MagnifyingGlassIcon className="mr-5 h-10" />
+      <Input
+        placeholder="Search for movies or TV series"
+        className="h-14 border-none text-xl"
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
